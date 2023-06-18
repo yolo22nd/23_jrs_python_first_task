c3d0f806da12e4bf0c0806af02cade8f9f8aad93
yolo22nd <130725553+yolo22nd@users.noreply.github.com>
2023-06-19 00:15:10 +0530

    Add files via upload

---
+++ om.py
@@ -0,0 +1,34 @@
+print("om tank")
+a = int(input("enter your number"))
+if a < 10:
+    print("single")
+elif a < 100:
+    print("double")
+elif a < 1000:
+    print("triple")
+else:
+    print("not any")
+
+
+b = input("enter number for fact")
+if b.isdigit():
+    b = int(b)
+    x = 1
+    for i in range(1,b+1):
+       x *= i
+    print(x)
+else:
+    print("invalid input")
+
+Computers={
+    "laptop1":{"brand" :"DELL","OS":"Windows"},
+    "laptop2":{"brand" :"HP" ,"OS":"Linux"},
+    "Desktop":{"brand" :"Apple" ,"OS":"Mac-OS"}
+}
+brands = []
+OS = []
+for i in Computers:
+    brands.append(Computers[i]["brand"])
+    OS.append(Computers[i]["OS"])
+print(brands)
+print(OS)
